af1df0698af263ed6a444e2048539e2ec8f586e9
aidanprice <justaidanp@gmail.com>
2022-07-12 16:49:53 +0100

    Added transparent map of service status to feed result
    A 'transparent map' from a Nagios service to a Feed result is a dumb mapping; it is a direct mapping of:
     - state_ok -> green
     - state_warning -> amber
     - state_critical -> red
     - state_unknown -> default
    With a direct mapping of `plugin_output` -> feed message
    
    This has been implemented as a simple way to turn a Nagios service into a feed

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,42 @@
 import { logger } from './logger';
 import { parse_nagios_status_file } from './parse_nagios_status_dat';
 import fs from 'fs';
+import ServiceStatus from "./nagios/service_status";
+
+interface FeedResult {
+  color: "green" | "amber" | "red" | "default",
+  message: string,
+  updated_at: Date
+}
+
+function map_service_to_transparent_feed(service: ServiceStatus): FeedResult | null {
+  if (service.state_type === "hard_state") {
+    let colour: "green" | "amber" | "red" | "default" = "default";
+    switch (service.current_state) {
+      case "state_ok":
+        colour = "green";
+        break;
+      case "state_warning":
+        colour = "amber";
+        break;
+      case "state_critical":
+        colour = "red";
+        break;
+      case "state_unknown":
+        colour = "default";
+        break;
+    }
+    return {
+      color: colour,
+      message: service.plugin_output,
+      updated_at: new Date()
+    };
+  }
+  // With a soft state, we should wait for nagios to turn to a hard state
+  else {
+    return null;
+  }
+}
 
 async function app() {
   const stream = fs.createReadStream(
@@ -10,7 +46,13 @@ async function app() {
   );
   const status_iter = parse_nagios_status_file(stream);
   for await (const status of status_iter) {
-    if (status !== null) logger.info(status);
+    if (status !== null) {
+      if (status.type === "ServiceStatus") {
+        logger.info(`Service Status with check_command ${status.status.check_command}`);
+        const feed_result = map_service_to_transparent_feed(status.status);
+        logger.info(feed_result, `Mapped service \`${status.status.service_description}\`@\`${status.status.host_name}\` to feed result`);
+      }
+    }
   }
 }
 
