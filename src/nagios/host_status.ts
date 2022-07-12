ea9a866bc83329be675e74bfb72ecb1fa6a11547
aidanprice <justaidanp@gmail.com>
2022-07-12 15:24:36 +0100

    Moved common parser utilities to separate file

--- src/nagios/host_status.ts
+++ src/nagios/host_status.ts
@@ -1,3 +1,11 @@
+import {
+  acknowledgement_type_to_enum,
+  check_option_to_enum,
+  check_type_number_to_enum,
+  state_num_to_enum,
+  state_type_to_enum,
+} from './utils';
+
 export default interface HostStatus {
   host_name: string;
 
@@ -100,119 +108,6 @@ export default interface HostStatus {
   //   if(temp_customvariablesmember->variable_name)
   //     fprintf(fp, "\t_%s=%d;%s\n", temp_customvariablesmember->variable_name, temp_customvariablesmember->has_been_modified, (temp_customvariablesmember->variable_value == NULL) ? "" : temp_customvariablesmember->variable_value);
   // }
-}
-
-// #define CHECK_TYPE_ACTIVE   0
-// #define CHECK_TYPE_PASSIVE  1
-// #define CHECK_TYPE_PARENT   2 /* (active) check for the benefit of dependent objects */
-// #define CHECK_TYPE_FILE     3 /* from spool files (yuck) */
-// #define CHECK_TYPE_OTHER    4 /* for modules to use */
-function check_type_number_to_enum(
-  check_type: string
-): 'active' | 'passive' | 'parent' | 'file' | 'other' {
-  switch (check_type) {
-    case '0':
-      return 'active';
-    case '1':
-      return 'passive';
-    case '2':
-      return 'parent';
-    case '3':
-      return 'file';
-    default:
-      return 'other';
-  }
-}
-// #define STATE_OK			0
-// #define STATE_WARNING			1
-// #define STATE_CRITICAL			2
-// #define STATE_UNKNOWN			3
-// #define STATE_UP                0
-// #define STATE_DOWN              1
-// #define STATE_UNREACHABLE       2
-// /* for legacy reasons */
-// #define HOST_UP              STATE_UP
-// #define HOST_DOWN            STATE_DOWN
-// #define HOST_UNREACHABLE     STATE_UNREACHABLE
-function state_num_to_enum(
-  state_num: string
-): 'state_ok' | 'state_warning' | 'state_critical' | 'state_unknown' {
-  switch (state_num) {
-    case '0':
-      return 'state_ok';
-    case '1':
-      return 'state_warning';
-    case '2':
-      return 'state_critical';
-    default:
-      return 'state_unknown';
-  }
-}
-
-// #define CHECK_OPTION_NONE		0	/* no check options */
-// #define CHECK_OPTION_FORCE_EXECUTION	1	/* force execution of a check (ignores disabled services/hosts, invalid timeperiods) */
-// #define CHECK_OPTION_FRESHNESS_CHECK    2       /* this is a freshness check */
-// #define CHECK_OPTION_ORPHAN_CHECK       4       /* this is an orphan check */
-// #define CHECK_OPTION_DEPENDENCY_CHECK   8       /* dependency check. different scheduling rules apply */
-function check_option_to_enum(
-  check_option: string
-):
-  | 'none'
-  | {
-      force_execution: boolean;
-      freshness_check: boolean;
-      orphan_check: boolean;
-      dependency_check: boolean;
-    } {
-  let byte = Number.parseInt(check_option);
-  if (byte == 0) {
-    return 'none';
-  } else {
-    let parsed = {
-      force_execution: false,
-      freshness_check: false,
-      orphan_check: false,
-      dependency_check: false,
-    };
-    if (byte & 0x01) {
-      parsed.force_execution = true;
-    }
-    if (byte & 0x02) {
-      parsed.freshness_check = true;
-    }
-    if (byte & 0x04) {
-      parsed.orphan_check = true;
-    }
-    if (byte & 0x08) {
-      parsed.dependency_check = true;
-    }
-    return parsed;
-  }
-}
-
-// #define SOFT_STATE			0
-// #define HARD_STATE			1
-function state_type_to_enum(state_type: string): 'soft_state' | 'hard_state' {
-  if (state_type == '0') {
-    return 'soft_state';
-  } else {
-    return 'hard_state';
-  }
-}
-
-// #define ACKNOWLEDGEMENT_NONE            0
-// #define ACKNOWLEDGEMENT_NORMAL          1
-// #define ACKNOWLEDGEMENT_STICKY          2
-function acknowledgement_type_to_enum(
-  acknowledgment_type: string
-): 'none' | 'normal' | 'sticky' {
-  if (acknowledgment_type == '0') {
-    return 'none';
-  } else if (acknowledgment_type == '1') {
-    return 'normal';
-  } else {
-    return 'sticky';
-  }
 }
 
 // Consumes lines from the status_file until a host status section has been parsed
