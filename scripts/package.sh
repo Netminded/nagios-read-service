3f54683bf88e135bc0eb155463f3ba0478151dab
aidanprice <justaidanp@gmail.com>
2022-07-21 13:14:06 +0100

    Added package script

---
+++ scripts/package.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# Performs a clean build
+rm -rf ./dist
+npm run build
+
+# Copies relevant files to a temporary directory
+mkdir /tmp/nagios-build
+cp ./README.md /tmp/nagios-build/README.md
+cp ./package.json /tmp/nagios-build/package.json
+cp ./package-lock.json /tmp/nagios-build/package-lock.json
+cp -r ./assets /tmp/nagios-build/assets
+cp -r ./dist /tmp/nagios-build/dist
+
+# Packages
+tar -C /tmp/nagios-build -czvf nagios-read-service-v1.0.0-beta.1.tar.gz .
+
+# Cleanup
+rm -rf /tmp/nagios-dist
